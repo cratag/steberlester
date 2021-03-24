3937141a0ac4f5a96b5474d2673deab45a0bf1ce
cratag <devcratag@gmail.com>
2021-03-24 12:48:56 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import navbar from './comp/navbar'
 
+
+import biglogo from "./assets/asd.webp"
+
 function App() {
   return (
     <div>
     {navbar()}
-    <Button>Hello
+    <section className="bg-light text-center">
+        <div className="container p-5">
+          <div className="card p-5">
+          <img src={biglogo} alt="" class="logo"></img>
+          <h4>Dedicados a productos de limpieza de alto desempeño y a insumos de seguridad e higiene de alta calidad</h4>
+          <Button className="btn btn-primary btn-lg" style={{width: "20%", alignSelf: "center", marginTop: "3%"}}>Contactanos</Button>
+          </div>
+        </div>
+    </section>
 
-    </Button>
-    <h1>Hello w23213orld</h1>
   </div>
   );
 }
 
 export default App;
+// https://mauritomeluso.wixsite.com/steberlester
--- src/comp/navbar.js
+++ src/comp/navbar.js
@@ -6,17 +6,18 @@ import logo from "../assets/logo.png"
 export default function navbar() {
   return (
   <div>
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" className="d-flex flex-row">
     <Navbar.Brand href="#home"><img src={logo} alt=""></img> <span style={{fontWeight: 'bold'}}> Steber Lester S.R.L</span></Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    <Nav>
+      <Nav.Link href="#home">Productos</Nav.Link>
+      <Nav.Link href="#features">¿Quienes somos?</Nav.Link>
+      <Nav.Link href="#pricing">Contacto</Nav.Link>
     </Nav>
-    <Form inline>
+    
+{/*     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-info">Search</Button>
-    </Form>
+    </Form> */}
   </Navbar>
   </div>
   );
